e7f6a1b46c5f88dd3ca813536e60ba812bd642ac
Charlie Xu <charlie.xu@pwc.com>
2019-11-28 17:48:09 +0000

    Additional test cases for TestReadForward3

--- enigma_test.go
+++ enigma_test.go
@@ -37,10 +37,22 @@ func TestReadForward3(t *testing.T) {
 	rotor1, rotor2, rotor3 := enigma.InitRotors3(baseWiring, baseWiring, baseWiring)
 	rotors := []*enigma.Rotor{rotor1, rotor2, rotor3}
 
-	result := enigma.ReadForward3(rotors, "A")
-	expectedResult := "T"
+	result1 := enigma.ReadForward3(rotors, "A")
+	expectedResult1 := "T"
 
-	if result != expectedResult {
-		t.Errorf("ReadForward3, got %v want %v", result, expectedResult)
+	result2 := enigma.ReadForward3(rotors, "F")
+	expectedResult2 := "F"
+
+	result3 := enigma.ReadForward3(rotors, "P")
+	expectedResult3 := "X"
+
+	if result1 != expectedResult1 {
+		t.Errorf("ReadForward3 case 1, got %v want %v", result1, expectedResult1)
+	}
+	if result2 != expectedResult2 {
+		t.Errorf("ReadForward3 case 2, got %v want %v", result2, expectedResult2)
+	}
+	if result3 != expectedResult3 {
+		t.Errorf("ReadForward3 case 3, got %v want %v", result3, expectedResult3)
 	}
 }
