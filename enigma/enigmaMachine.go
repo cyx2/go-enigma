3613a6a0cf3531c7320206b5a180e072b2e5079d
Charlie Xu <xucharliey@gmail.com>
2020-05-12 09:57:44 -0400

    Added function ProcessLetter

--- enigma/enigmaMachine.go
+++ enigma/enigmaMachine.go
@@ -4,3 +4,16 @@ package enigma
 func (rotor *Rotor) Rotate() {
 	rotor.offset++
 }
+
+// ProcessLetter will process a single enigma letter
+func (machine *Machine) ProcessLetter(letter string) (processedLetter string) {
+	machineRotors := machine.rotors
+	machineReflector := machine.reflector
+	processForward := ReadForward(machineRotors, letter)
+	processReflector := ReadReflector(machineReflector, processForward)
+	processBackward := ReadBackward(machineRotors, processReflector)
+
+	processedLetter = processBackward
+
+	return processedLetter
+}
--- enigmaMachine_test.go
+++ enigmaMachine_test.go
@@ -6,6 +6,34 @@ import (
 )
 
 func TestMachineInit(t *testing.T) {
+	baseWiring1 := "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+	baseWiring2 := "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+	baseWiring3 := "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+	reflBaseWiring := "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+
+	newMachine := enigma.InitMachine(baseWiring1, baseWiring2, baseWiring3, reflBaseWiring)
+
+	rotor1Index := 0
+	newMachineRotor1Wiring := enigma.GetMachineRotorWiring(newMachine, rotor1Index)
+
+	if newMachineRotor1Wiring != baseWiring1 {
+		t.Errorf("For rotor %v got wiring of %v, want %v", rotor1Index, newMachineRotor1Wiring, baseWiring1)
+	}
+
+	rotor3Index := 2
+	rotor3Offset := 0
+	newMachineRotor3Wiring := enigma.GetMachineRotorWiring(newMachine, rotor3Index)
+	newMachineRotor3Offset := enigma.GetMachineRotorOffset(newMachine, rotor3Index)
+
+	if newMachineRotor3Wiring != baseWiring3 {
+		t.Errorf("For rotor %v got wiring of %v, want %v", rotor3Index, newMachineRotor3Wiring, baseWiring3)
+	}
+	if newMachineRotor3Offset != rotor3Offset {
+		t.Errorf("For rotor %v got offset of %v, want %v", rotor3Index, newMachineRotor3Offset, rotor3Offset)
+	}
+}
+
+func TestProcessLetter(t *testing.T) {
 	// Alphabet:   ABCDEFGHIJKLMNOPQRSTUVWXYZ
 	// Rotor Base1:EKMFLGDQVZNTOWYHXUSPAIBRCJ
 
@@ -33,22 +61,19 @@ func TestMachineInit(t *testing.T) {
 
 	newMachine := enigma.InitMachine(baseWiring1, baseWiring2, baseWiring3, reflBaseWiring)
 
-	rotor1Index := 0
-	newMachineRotor1Wiring := enigma.GetMachineRotorWiring(newMachine, rotor1Index)
+	testLetter1 := "A"
+	processedLetter1 := newMachine.ProcessLetter(testLetter1)
+	expProcessedLetter1 := "K"
 
-	if newMachineRotor1Wiring != baseWiring1 {
-		t.Errorf("For rotor %v got wiring of %v, want %v", rotor1Index, newMachineRotor1Wiring, baseWiring1)
+	testLetter2 := "Z"
+	processedLetter2 := newMachine.ProcessLetter(testLetter2)
+	expProcessedLetter2 := "E"
+
+	if processedLetter1 != expProcessedLetter1 {
+		t.Errorf("Tried to process %v, got %v, want %v", testLetter1, processedLetter1, expProcessedLetter1)
 	}
 
-	rotor3Index := 2
-	rotor3Offset := 0
-	newMachineRotor3Wiring := enigma.GetMachineRotorWiring(newMachine, rotor3Index)
-	newMachineRotor3Offset := enigma.GetMachineRotorOffset(newMachine, rotor3Index)
-
-	if newMachineRotor3Wiring != baseWiring3 {
-		t.Errorf("For rotor %v got wiring of %v, want %v", rotor3Index, newMachineRotor3Wiring, baseWiring3)
-	}
-	if newMachineRotor3Offset != rotor3Offset {
-		t.Errorf("For rotor %v got offset of %v, want %v", rotor3Index, newMachineRotor3Offset, rotor3Offset)
+	if processedLetter2 != expProcessedLetter2 {
+		t.Errorf("Tried to process %v, got %v, want %v", testLetter2, processedLetter2, expProcessedLetter2)
 	}
 }
